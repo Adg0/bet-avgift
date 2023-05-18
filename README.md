b309eb35ebeade3d310c4a6142b4c594ccbe9b49
3Spear <kalebbirru@gmail.com>
2023-05-18 23:47:43 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AA Taxable wins
+
+
---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.PROVIDER_API}`,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
+};
